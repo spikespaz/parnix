2e5432d0ca44137d88c790e6160eae0f8af758e6
Jacob Birkett <spikespaz@outlook.com>
2023-05-14 01:35:42 -0700

    flake+module: add comments

--- flake.nix
+++ flake.nix
@@ -1,17 +1,41 @@
 {
   outputs = inputs@{ self, nixpkgs, ... }: {
-    lib = { partitionConfiguration = import ./module/eval-config.nix; };
+    # In your flake, create `partitionConfigurations` outputs by calling
+    # `inputs.parnix.lib.partitionConfiguration`, and specify arguments defined
+    # by `./module/eval-config.nix`.
+    lib.partitionConfiguration = import ./module/eval-config.nix;
     nixosModules.parnix = import ./module.nix;
 
-    partitionConfigurations.simple = self.lib.partitionConfiguration {
+    # A very simple example configuration
+    # for a standard system with an SSD.
+    partitionConfigurations.simple-ssd = self.lib.partitionConfiguration {
       inherit nixpkgs;
+      # Because `pkgs` needs instantiation and this configuration
+      # is specific to a host, specify the system this config
+      # will be used on.
+      # This is the default value.
+      system = "x86_64-linux";
+      # The parnix configuration could be written directly here,
+      # however I find it convenient to import as if it were a template.
       config = import ./examples/simple.nix;
+      # Then any disk-specific options (or system-specific) can go here,
+      # which is specific to the "simple" configuration,
+      # which in a real flake would be associated with a host.
       specialArgs.primaryDevice = {
+        # This is a configuration for a Western Digital 2 TiB SN770.
+        # Normally this would be `/dev/nvme0n1`.
         path = "/dev/sda";
-        # blockdev --getsize64 /dev/sda
+        # Command to get the size in bytes for a drive:
+        # `blockdev --getsize64 /dev/sda`
+        # Then divide by 1024 to get KiB (kibibytes).
         size = 2000398934016 / 1024;
+        # This is a 5% reserved free space at the end of the drive.
+        # Good for solid state.
         reservePercent = 5.0e-2;
-        # awk '{if ($1 == "MemTotal:") print $2}' /proc/meminfo
+        # Command to get the mount of physical memory in KiB:
+        # `awk '{if ($1 == "MemTotal:") print $2}' /proc/meminfo`
+        # Two gibibytes extra is added so that uncompressed hibernation
+        # does not fail.
         swapSize = 38735612 + 2 * 1024 * 1024;
       };
     };
--- module/eval-config.nix
+++ module/eval-config.nix
@@ -1,20 +1,26 @@
 {
 # The system to use to import nixpkgs.
 system ? "x86_64-linux",
-# Nixpkgs input.
-nixpkgs,
+# Nixpkgs input. Thiswill only be used if `pkgs` is unspecified.
+nixpkgs ? null,
+# Custom instance of `pkgs`, for example, with overlays.
+pkgs ? nixpkgs.legacyPackages.${system},
 # The config to evaluate. Should contain top level `parnix`.
+# Note that this is required, however a default is provided
+# so that the `extraModules` list may be used instead,
+# as a convenience.
 config ? ({ ... }: { }),
 # Extra modules to evaluate.
 extraModules ? [ ],
 # Special argument to pass to modules.
+# If `lib` is provided in this attrset
+# it will be used instead of `nixpkgs.lib`.
 specialArgs ? { }
   #
 }:
 let
-  lib = specialArgs.lib or nixpkgs.lib;
+  lib = specialArgs.lib or pkgs.lib or nixpkgs.lib;
   parnixLib = lib.extend (import ./lib.nix);
-  pkgs = nixpkgs.legacyPackages.${system};
 
   evaluated = lib.evalModules {
     modules = [ config ./. ] ++ extraModules;
